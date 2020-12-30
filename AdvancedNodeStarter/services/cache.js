db8ee453012de42cbfedb8d23ed174da856495f9
Sreeganesh <ganuiscool@gmail.com>
2020-12-30 17:58:38 +0530

    Basic Key creation for cache

---
+++ AdvancedNodeStarter/services/cache.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+
+const exec = mongoose.Query.prototype.exec;
+mongoose.Query.prototype.exec = function () {
+        console.log("i am about to run ")
+      const key =  Object.assign({},this.getQuery(),{
+         collection: this.mongooseCollection.name
+        });
+        console.log(key);
+        return exec.apply(this,arguments);
+}
+
